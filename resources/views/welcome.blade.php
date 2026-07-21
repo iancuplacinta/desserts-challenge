185ba069a019a762e58caa52fee80effcbed0206
Iancu Placinta <iplacinta@amherst.edu>
2026-07-21 10:59:25 -0400

    updated layout and styling of products

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,37 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Frontend Mentor | Product list with cart</title>
+    <title>Frontend Mentor | Product list with cart</title>
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    </head>
-    <body>
-        <div class="flex gap-4 p-8">
-            <div class="size-24 rounded-lg bg-red"></div>
-            <div class="size-24 rounded-lg bg-green"></div>
-            <div class="size-24 rounded-lg bg-rose-50"></div>
-            <div class="size-24 rounded-lg bg-rose-100"></div>
-            <div class="size-24 rounded-lg bg-rose-300"></div>
-            <div class="size-24 rounded-lg bg-rose-400"></div>
-            <div class="size-24 rounded-lg bg-rose-500"></div>
-            <div class="size-24 rounded-lg bg-rose-900"></div>
-        </div>
-        <p class="text-4xl">Testing the new font!</p>
+</head>
+<body class="max-w-360 mx-auto px-4 sm:px-6 lg:px-8 grid md:grid-cols-[1fr_400px] gap-8 bg-rose-50 py-16">
+    <main>
+        <h1 class="text-5xl font-bold">Desserts</h1>
 
-        <div class="p-8">
-            <ul>
-                @foreach($products as $product)
-                    <li>{{ $product->name }}</li>
-                @endforeach
-            </ul>
-        </div>
-    </body>
+        <ul class="mt-8 grid sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+            @foreach($products as $product)
+                <x-product :product="$product"/>
+            @endforeach
+
+        </ul>
+    </main>
+    
+    <aside class="bg-white p-6 h-80">Shopping Cart</aside>
+</body>
 </html>
