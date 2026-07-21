13cd934afb4ce29b6ff7a8430997239a45bc6c56
Iancu Placinta <iplacinta@amherst.edu>
2026-07-21 10:19:34 -0400

    create model, migration and seeder for products

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,5 +25,13 @@
             <div class="size-24 rounded-lg bg-rose-900"></div>
         </div>
         <p class="text-4xl">Testing the new font!</p>
+
+        <div class="p-8">
+            <ul>
+                @foreach($products as $product)
+                    <li>{{ $product->name }}</li>
+                @endforeach
+            </ul>
+        </div>
     </body>
 </html>
